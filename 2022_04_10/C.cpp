fafca0623a693df9fc4de25ee820ec5ca07b7c64
yilm01 <philosophia268@gmail.com>
2022-04-11 02:48:57 +0900

    左が表示されない。

--- 2022_04_10/C.cpp
+++ 2022_04_10/C.cpp
@@ -1,24 +1,26 @@
 #include<iostream>
 using namespace std;
 
-int S(int n, int* hoge)
+int S(int n)
 {
-  if(n==1) return hoge[0]=1;
+  if(n==1)
+  {
+    return 1;
+  }
   else
   {
-  return S(n-1, hoge) + n + S(n-1, hoge);
+    cout<<S(n-1);
+    cout<<n;
+    cout<<S(n-1);
+    cout<<endl;
   }
+  return 0;
 }
 
 int main()
 {
   int N;
-  int hoge[100]={0};
   cin>>N;
-  S(N, hoge);
-
-
-  cout<<S(N, hoge)<<endl;
-
+  S(N);
   return 0;
 }
